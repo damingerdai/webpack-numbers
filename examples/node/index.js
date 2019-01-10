b444d29b7cf9183fb232d8adc70bb412e3439e45
明国宾 <mingguobin@live.com>
2019-01-10 20:34:51 +0800

    feat: 去除对lodash显式引用

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,5 +1,5 @@
 // https://github.com/kalcifer/webpack-library-example/blob/master/examples/node/example.js
-require('lodash');
+// require('lodash');
 var webpackNumbers = require('./webpack-numbers.js');
 var out = () => {
     process.stdout.write('This is the result for numtoword(1) === ' + webpackNumbers.numToWord(1));
